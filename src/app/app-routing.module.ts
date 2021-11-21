d118a959f3880d81097cca0c4b1947217771f481
IvanUlloa098 <iulloa@est.up.edu.ec>
2021-11-21 12:56:41 -0500

    Creacion de la pagina Transaction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'transaction',
+    loadChildren: () => import('./transaction/transaction.module').then( m => m.TransactionPageModule)
   }
 ];
 
