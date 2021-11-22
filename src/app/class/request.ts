b9dd59179c44ce70515cf70f3cd61e8f512012a3
IvanUlloa098 <iulloa@est.up.edu.ec>
2021-11-21 19:29:38 -0500

    Pruebas de ESB

---
+++ src/app/class/request.ts
@@ -0,0 +1,4 @@
+export class RequestESB {
+    id:number
+    nombre:string
+  }
--- src/app/transaction/transaction.page.ts
+++ src/app/transaction/transaction.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestESB } from '../class/request';
+import { ESBserviceService } from '../services/esbservice.service';
 
 @Component({
   selector: 'app-transaction',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionPage implements OnInit {
 
-  constructor() { }
+  private bankInfo: any
+  private aux = new RequestESB()
+
+  constructor(private esbService : ESBserviceService) { 
+    this.aux.id = 1
+    this.aux.nombre = "payload"
+
+    this.bankInfo = this.esbService.startTransaction(this.aux).subscribe((result) => {
+      // This code will be executed when the HTTP call returns successfully 
+    });
+    
+  }
 
   ngOnInit() {
+    
+  }
+
+  getBankInformation() {
+    
+    
   }
 
 }
